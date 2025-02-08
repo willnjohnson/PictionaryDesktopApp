3858122fe0aae7e39fc6c36cb701f7f19c687e2c
William Johnson <wnj@tutamail.com>
2025-02-07 21:53:13 -0500

    First commit through Visual Studio. Made a simple lobby.

---
+++ PictionaryDesktopApp/Views/GameLobbyView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace PictionaryDesktopApp.Views
+{
+    public partial class GameLobbyView : Window
+    {
+        public GameLobbyView()
+        {
+            InitializeComponent(); // This links the XAML layout to the code-behind
+        }
+
+        private void HostGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Your code to host a game goes here
+            MessageBox.Show("Hosting Game...");
+        }
+
+        private void JoinGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Your code to join a game goes here
+            MessageBox.Show("Joining Game...");
+        }
+    }
+}
